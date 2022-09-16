d42c5e3348d814cc57cba93cb166533015d58cb9
npsaunders <neilpsaunders@gmail.com>
2022-09-15 23:57:36 -0400

    fix show page issue

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,24 +1,16 @@
-const Recipe = require('../models/recipes.js');
+// const Recipe = require('../models/recipes.js');
 
-module.exports = {
-  index,
-  addFact,
-  delFact
-};
+// module.exports = {
+//   index,
+//   addFact,
+//   delFact
+// };
 
-function index(req, res) {
-  Recipe.find({}, function (err, recipes) {
-    res.render('recipes/index', {
-      recipes,
-      user: req.user
-    });
-  });
-}
-
-function addFact(req, res) {
-
-}
-
-function delFact(req, res) {
-
-}
+// function index(req, res) {
+//   Recipe.find({}, function (err, recipes) {
+//     res.render('recipes/index', {
+//       recipes,
+//       user: req.user
+//     });
+//   });
+// }
